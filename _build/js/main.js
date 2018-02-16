014001d33d24a37e0ac4d0c6e04a3b1504393819
jdaehne <jan.daehne@quadro-system.de>
2018-02-16 11:49:48 +0100

    add clickable-parent js functionality

--- _build/js/main.js
+++ _build/js/main.js
@@ -1,11 +1,29 @@
-$(document).ready(function() { 
+$(document).ready(function() {
 
 
 	// * * * * * * * * * * * * * * * * * * * * * * * * *
 	// * Name
-	// * 
+	// *
 	// *
 
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * *
+    // * parent clickable elements (excludes links inside)
+    // *
+    // * @set outer parent element class: js-click-item-parent
+    // * @set link (<a> tag) element class: js-click-item-link
+    // *
+    // * This makes the whole element clickable and still
+    // * makes other links inside clickable with their target
+    // *
+    $(".js-click-item-parent").delegate('a', 'click', function(e){
+        window.location = $(this).attr("href");
+        return false;
+    }).click(function(){
+        window.location = $(this).find("a.js-click-item-link").attr("href");
+        return false;
+    });​
 
 
 
