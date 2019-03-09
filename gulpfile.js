911e464e66ed4191745ddefd5a8e2ba11dab4a7d
jdaehne <jan.daehne@quadro-system.de>
2019-03-09 15:34:38 +0100

    Gulp Upgrade to 4.x

--- gulpfile.js
+++ gulpfile.js
@@ -3,92 +3,95 @@ var gulp = require('gulp'),
 	imagemin = require('gulp-imagemin'),
 	plumber = require('gulp-plumber'),
 	browserSync = require('browser-sync').create(),
-	uglify = require('gulp-uglifyjs'),
+	uglify = require('gulp-uglify'),
 	sass = require('gulp-sass'),
 	svgstore = require('gulp-svgstore'),
 	svgmin = require('gulp-svgmin'),
 	rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
 	gcmq = require('gulp-group-css-media-queries'),
-	csso = require('gulp-csso');
+	csso = require('gulp-csso')
+    sourcemaps = require('gulp-sourcemaps');
 
 
-// Static Browser:
-gulp.task('serve', ['styles'], function() {
-    browserSync.init({
-        server: "./",
-        notify: false
-    });
-});
-
-
-
-// Styles Task
-gulp.task('styles', function(){
-	gulp.src('_build/scss/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(prefix('last 4 versions'))
-		.pipe(gcmq())
+// styles
+function styles(done) {
+    gulp.src('_build/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(prefix('last 4 versions'))
+        .pipe(gcmq())
         .pipe(csso())
-		.pipe(plumber())
-		.pipe(gulp.dest('css/'))
-		.pipe(browserSync.stream());
-});
+        .pipe(plumber())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('css/'))
+        .pipe(browserSync.stream());
+    done();
+}
 
-
-// JS Task
-gulp.task('scripts', function(){
-	gulp.src([
-			'!_build/js/modernizr*.js',
+// scripts
+function scripts(done) {
+    gulp.src([
 			'_build/js/jquery*.js',
 			'_build/js/!(main)*.js',
 			'_build/js/main.js'
 		])
+        .pipe(sourcemaps.init())
 		.pipe(plumber())
-		.pipe(uglify('main.min.js', {
-			outSourceMap: true
-		}))
+        .pipe(concat('main.min.js'))
+		.pipe(uglify())
+        .pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('js/'))
 		.pipe(browserSync.stream());
-});
+    done();
+}
 
+// svg
+function svg(done) {
+    gulp.src('_build/svg/*.svg')
+		.pipe(svgmin())
+		.pipe(svgstore())
+		.pipe(rename({basename: 'sprite'}))
+		.pipe(gulp.dest('img/'));
+    done();
+}
 
-
-
-// Image Task
-gulp.task('image', function(){
-	gulp.src('_build/img/*')
+// images
+function images(done) {
+    gulp.src('_build/img/*')
 		.pipe(imagemin())
 		.pipe(gulp.dest('img/'));
 	gulp.src('_build/uploads/*')
 		.pipe(imagemin())
 		.pipe(gulp.dest('uploads/'));
-});
+    done();
+}
 
+// browsersync
+function browser_sync(done) {
+    browserSync.init({
+        server: "./",
+        notify: false
+    });
+    done();
+}
 
+// html
+function html(done) {
+    gulp.src('./*.html')
+		.pipe(browserSync.stream());
+    done();
+}
 
-// SVG Task
-gulp.task('svg', function(){
-	gulp.src('_build/svg/*.svg')
-		.pipe(svgmin())
-		.pipe(svgstore())
-		.pipe(rename({basename: 'sprite'}))
-		.pipe(gulp.dest('img/'));
-});
+// watch
+function watch() {
+    gulp.watch('_build/scss/**/*.scss', styles);
+    gulp.watch('_build/js/*.js', scripts);
+    gulp.watch('_build/img/*', images);
+    gulp.watch('_build/uploads/*', images);
+    gulp.watch('_build/svg/*.svg', svg);
+    gulp.watch('./*.html', html);
+}
 
-
-
-
-// Watch Task
-gulp.task('watch', function() {
-	gulp.watch('_build/scss/**/*.scss', ['styles']).on('change', browserSync.reload);
-	gulp.watch('_build/js/*.js', ['scripts']).on('change', browserSync.reload);
-	gulp.watch('_build/img/*', ['image']);
-	gulp.watch('_build/uploads/*', ['image']);
-	gulp.watch('_build/svg/*.svg', ['svg']);
-	gulp.watch("./*.html").on('change', browserSync.reload);
-});
-
-
-
-
-gulp.task('default', ['styles', 'scripts', 'image', 'svg', 'watch', 'serve']);
+// default
+gulp.task("default", gulp.parallel(browser_sync, styles, scripts, svg, images, watch));
